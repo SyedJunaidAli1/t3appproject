b51d731be7bb9dcab6de6ceefa2502f967f8ab6a
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2024-10-21 14:01:04 +0530

    force dynamic + vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic"
+
 const mockUrls = [
   "https://utfs.io/f/ad98a8a5-ad14-4040-b763-b3dcd7e2bf46-9ahyd4.29.11.06.png",
   "https://utfs.io/f/0982b479-e6d9-48b8-85cd-4f503f53e55f-b6ofwv.55.33.31.png",
@@ -13,12 +15,15 @@ const mockImages = mockUrls.map((url, index) => ({
 
 
 export default async function HomePage() {
+  
   const post = await db.query.posts.findMany()
+
   console.log(post);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {post.map ((post) => (
+        {post.map((post) => (
           <div key={post.id}>{post.name}</div>
         ))}
         {[...mockImages, ...mockImages, ...mockImages,].map((imges, index) => (
