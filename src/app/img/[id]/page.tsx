53ea6f773b5fd87dfbbd898a2f99b34c81431857
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2024-11-04 13:50:30 +0530

    Delete button w/server actions

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,15 +1,13 @@
 import FullPageImageView from "~/common/full-image-page";
 
-export default function PhotoPage({
-    params: { id: photoId },
+export default function PhotoModal({
+  params: { id: photoId },
 }: {
-    params: { id: string };
+  params: { id: string };
 }) {
-    const idAsNumber = Number(photoId)
-    if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id")
-    return (
-        <>
-            <FullPageImageView id={idAsNumber} />
-        </>
-    )
+  return (
+    <div className="flex h-full min-h-0 w-full min-w-0 overflow-y-hidden">
+      <FullPageImageView photoId={photoId} />
+    </div>
+  );
 }
--- src/common/full-image-page.tsx
+++ src/common/full-image-page.tsx
@@ -1,23 +1,41 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { getImage } from "~/server/queries";
+import { Button } from "~/components/ui/button";
+import { deleteImage, getImage } from "~/server/queries";
 
-export default async function FullPageImageView(props: { id: number }) {
-    const image = await getImage(props.id)
-    const uploaderinfo = await clerkClient.users.getUser(image.userId);
+export default async function FullPageImageView(props: { photoId: string }) {
+    const idAsNumber = Number(props.photoId)
+    // if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id")
+
+    const image = await getImage(idAsNumber)
+    const userInfo = await clerkClient.users.getUser(image.userId)
     return (
-        <div className="flex w-full h-full min-w-0 ">
-            <div className="flex-shrink flex justify-center items-center">
-                <img src={image.url} className="object-contain flex-shrink" />
+        <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+            <div className="flex-shrink flex-grow">
+                <img src={image.url} className="object-contain" />
             </div>
-            <div className="w-48 flex flex-col flex-shrink-0 border-l">
-                <div className="text-lg border-b text-center p-2">{image.name}</div>
-                <div className="flex flex-col p-2">
-                    <span>Uploaded By:</span>
-                    <span>{uploaderinfo.fullName}</span>
+
+            <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
+                <div className="border-b p-2 text-center text-xl">
+                    {image.name}
                 </div>
-                <div className="flex flex-col p-2">
-                    <span>Created On:</span>
-                    <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+
+                <div className="p-2">
+                    <div>Uploaded By:</div>
+                    <div>{userInfo.fullName}</div>
+                </div>
+
+                <div className="p-2">
+                    <div>Created On:</div>
+                    <div>{image.createdAt.toLocaleDateString()}</div>
+                </div>
+
+                <div className="p-2">
+                    <form action={async () => {
+                        "use server"
+                        await deleteImage(idAsNumber)
+                    }}>
+                        <Button variant="destructive" type="submit">Delete</Button>
+                    </form>
                 </div>
             </div>
         </div>
